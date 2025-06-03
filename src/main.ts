9c11e9a1d2958da4cc650c0fa49af56fe09bd27d
lemoissonM <murhulametre@gmail.com>
2025-06-03 09:07:44 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    credentials: true,
   });
   await app.listen(process.env.PORT ?? 8000);
 }
